9ef2499938fac0dc4b8842a3a9b4e467f8fd187d
Taro L. Saito <leo@xerial.org>
2013-03-18 16:15:04 +0900

    Add UInt32Array

--- src/main/scala/xerial/larray/MemoryAllocator.scala
+++ src/main/scala/xerial/larray/MemoryAllocator.scala
@@ -106,6 +106,12 @@ object UnsafeUtil extends Logger {
   private val addressFactor = if(addressBandWidth == 64) 8L else 1L
   val addressSize = unsafe.addressSize()
 
+  /**
+   * @param obj
+   * @return
+   *
+   */
+  @deprecated(message="Deprecated because this method does not return correct object addresses in some platform", since="0.1")
   def getObjectAddr(obj:AnyRef) : Long = {
     trace(f"address factor:$addressFactor%d, addressSize:$addressSize, objectArrayOffset:$objectArrayOffset, objectArrayScale:$objectArrayScale")
 
---
+++ src/main/scala/xerial/larray/UInt32Array.scala
@@ -0,0 +1,35 @@
+//--------------------------------------
+//
+// UInt32Array.scala
+// Since: 2013/03/18 3:57 PM
+//
+//--------------------------------------
+
+package xerial.larray
+
+/**
+ * Array of uint32 values. The internal array representation is the same with LIntArray, but the apply and update methods are based on Long type values.
+ *
+ * @author Taro L. Saito
+ */
+class UInt32Array(val size: Long, val address: Long)(implicit mem: MemoryAllocator) extends LArray[Long] with UnsafeArray[Long] {
+  def this(size:Long)(implicit mem: MemoryAllocator) = this(size, mem.allocate(size << 2))
+
+  import UnsafeUtil.unsafe
+
+  def apply(i:Long) : Long = {
+    val v : Long = unsafe.getInt(address + (i << 2)) & 0xFFFFFFFFL
+    v
+  }
+
+  def update(i:Long, v:Long) : Long = {
+    unsafe.putInt(address + (i << 2), (v & 0xFFFFFFFFL).toInt)
+    v
+  }
+
+  /**
+   * byte length of this array
+   * @return
+   */
+  def byteLength = size * 4L
+}
