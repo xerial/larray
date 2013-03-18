45d19ebc29b46c5ca35aad45c58938a53a8611bf
Taro L. Saito <leo@xerial.org>
2013-03-18 14:31:24 +0900

    fix comment

--- src/main/scala/xerial/larray/impl/LArrayLoader.scala
+++ src/main/scala/xerial/larray/impl/LArrayLoader.scala
@@ -19,37 +19,29 @@ import java.util.Properties
 
 /**
  * <b>Internal only - Do not use this class.</b> This class loads a native
- * library of snappy-java (snappyjava.dll, libsnappy.so, etc.) according to the
- * user platform (<i>os.name</i> and <i>os.arch</i>). The natively compiled
- * libraries bundled to snappy-java contain the codes of the original snappy and
- * JNI programs to access Snappy.
- *
- * In default, no configuration is required to use snappy-java, but you can load
+ * library of larray (larray.dll, liblarray.so, etc.) according to the
+ * user platform (<i>os.name</i> and <i>os.arch</i>).
+
+ * In default, no configuration is required to use larray, but you can load
  * your own native library created by 'make native' command.
  *
- * This SnappyLoader searches for native libraries (snappyjava.dll,
- * libsnappy.so, etc.) in the following order:
+ * This LArrayLoader load a native library (larray.dll, larray.so, etc.) using the following procedure:
  * <ol>
- * <li>If system property <i>org.xerial.snappy.use.systemlib</i> is set to true,
- * lookup folders specified by <i>java.lib.path</i> system property (This is the
- * default path that JVM searches for native libraries)
- * <li>(System property: <i>org.xerial.snappy.lib.path</i>)/(System property:
- * <i>org.xerial.lib.name</i>)
- * <li>One of the libraries embedded in snappy-java-(version).jar extracted into
+ * <li>Extract one of the libraries embedded in larray-(version).jar into
  * (System property: <i>java.io.tempdir</i>). If
- * <i>org.xerial.snappy.tempdir</i> is set, use this folder instead of
+ * <i>xerial.larray.tempdir</i> is set, use this folder instead of
  * <i>java.io.tempdir</i>.
  * </ol>
  *
  * <p>
  * If you do not want to use folder <i>java.io.tempdir</i>, set the System
- * property <i>org.xerial.snappy.tempdir</i>. For example, to use
+ * property <i>xerial.larray.tempdir</i>. For example, to use
  * <i>/tmp/leo</i> as a temporary folder to copy native libraries, use -D option
  * of JVM:
  *
  * <pre>
  * <code>
- * java -Dorg.xerial.snappy.tempdir="/tmp/leo" ...
+ * java -Dxerial.larray.tempdir="/tmp/leo" ...
  * </code>
  * </pre>
  *
@@ -106,36 +98,36 @@ object LArrayLoader {
     }
 
     /**
-     * Load SnappyNative and its JNI native implementation using the root class
+     * Load LArrayNative and its JNI native implementation using the root class
      * loader. This hack is for avoiding the JNI multi-loading issue when the
      * same JNI library is loaded by different class loaders.
      * 
      * In order to load native code in the root class loader, this method first
-     * inject SnappyNativeLoader class into the root class loader, because
+     * inject LArrayNativeLoader class into the root class loader, because
      * {@link System#load(String)} method uses the class loader of the caller
      * class when loading native libraries.
      * 
      * <pre>
-     * (root class loader) -> [SnappyNativeLoader (load JNI code), SnappyNative (has native methods), SnappyNativeAPI, SnappyErrorCode]  (injected by this method)
+     * (root class loader) -> [LArrayNativeLoader (load JNI code), LArrayNative (has native methods), LArrayNativeAPI]  (injected by this method)
      *    |
      *    |
      * (child class loader) -> Sees the above classes loaded by the root class loader.
-     *   Then creates SnappyNativeAPI implementation by instantiating SnappyNaitive class.
+     *   Then creates LArrayNativeAPI implementation by instantiating LArrayNaitive class.
      * </pre>
      * 
      * 
      * <pre>
-     * (root class loader) -> [SnappyNativeLoader, SnappyNative ...]  -> native code is loaded by once in this class loader 
+     * (root class loader) -> [LArrayNativeLoader, LArrayNative ...]  -> native code is loaded by once in this class loader
      *   |   \
      *   |    (child2 class loader)      
      * (child1 class loader)
      * 
-     * child1 and child2 share the same SnappyNative code loaded by the root class loader.
+     * child1 and child2 share the same LArrayNative code loaded by the root class loader.
      * </pre>
      * 
      * Note that Java's class loader first delegates the class lookup to its
-     * parent class loader. So once SnappyNativeLoader is loaded by the root
-     * class loader, no child class loader initialize SnappyNativeLoader again.
+     * parent class loader. So once LArrayNativeLoader is loaded by the root
+     * class loader, no child class loader initialize LArrayNativeLoader again.
      * 
      * @return
      */
@@ -151,13 +143,13 @@ object LArrayLoader {
       def injectNativeLoader : Class[_] = {
 
         try {
-          // Use parent class loader to load SnappyNative, since Tomcat, which uses different class loaders for each webapps, cannot load JNI interface twice
+          // Use parent class loader to load LArrayNative, since Tomcat, which uses different class loaders for each webapps, cannot load JNI interface twice
 
           val nativeLoaderClassName = "xerial.larray.LArrayNativeLoader"
           val rootClassLoader = getRootClassLoader
           // Load a byte code
           val byteCode = getByteCode("/xerial/larray/LArrayNativeLoader.bytecode")
-          // In addition, we need to load the other dependent classes (e.g., SnappyNative and SnappyException) using the system class loader
+          // In addition, we need to load the other dependent classes (e.g., LArrayNativeAPI and LArrayNative) using the system class loader
           val classesToPreload = Array("xerial.larray.impl.LArrayNativeAPI", "xerial.larray.impl.LArrayNative")
           val preloadClassByteCode = for (each <- classesToPreload) yield getByteCode(String.format("/%s.class", each.replaceAll("\\.", "/")))
 
@@ -183,7 +175,7 @@ object LArrayLoader {
             defineClass.setAccessible(false)
           }
 
-          // Load the SnappyNativeLoader class
+          // Load the LArrayNativeLoader class
           rootClassLoader.loadClass(nativeLoaderClassName)
         }
         catch {
@@ -206,7 +198,7 @@ object LArrayLoader {
           }
 
           isLoaded = true;
-          // Look up SnappyNative, injected to the root classloder, using reflection in order to avoid the initialization of SnappyNative class in this context class loader.
+          // Look up LArrayNative, injected to the root classloder, using reflection in order to avoid the initialization of LArrayNative class in this context class loader.
           val nativeCode : Any = Class.forName("xerial.larray.impl.LArrayNative").newInstance()
           api = nativeCode
           api
@@ -220,8 +212,8 @@ object LArrayLoader {
     }
 
     /**
-     * Load snappy-java's native code using load method of the
-     * SnappyNativeLoader class injected to the root class loader.
+     * Load LArray's native code using the load method of the
+     * LArrayNativeLoader class injected to the root class loader.
      * 
      * @param loaderClass
      * @throws SecurityException
@@ -236,11 +228,11 @@ object LArrayLoader {
 
       findNativeLibrary match {
         case Some(nativeLib) =>
-            // Load extracted or specified snappyjava native library.
+            // Load extracted or specified larray native library.
             val loadMethod = loaderClass.getDeclaredMethod("load", classOf[String])
             loadMethod.invoke(null, nativeLib.getAbsolutePath().asInstanceOf[AnyRef])
         case None =>
-          // Load preinstalled snappyjava (in the path -Djava.library.path)
+          // Load preinstalled larray (in the path -Djava.library.path)
           val loadMethod = loaderClass.getDeclaredMethod("loadLibrary", classOf[String])
           loadMethod.invoke(null, "larray");
       }
@@ -351,8 +343,8 @@ object LArrayLoader {
         if(OSInfo.getOSName().equals("Mac")) {
           val altName = "liblarray.jnilib";
           if(hasResource(nativeLibraryPath + "/" + altName)) {
-            nativeLibraryName = altName;
-            hasNativeLib = true;
+            nativeLibraryName = altName
+            hasNativeLib = true
           }
         }
       }
@@ -362,7 +354,7 @@ object LArrayLoader {
         sys.error(errorMessage)
       }
 
-      // Temporary library folder. Use the value of org.xerial.snappy.tempdir or java.io.tmpdir
+      // Temporary library folder. Use the value of xerial.larray.tempdir or java.io.tmpdir
       val tempFolder = new File(System.getProperty(KEY_LARRAY_TEMPDIR, System.getProperty("java.io.tmpdir"))).getAbsolutePath()
       // Extract and load a native library inside the jar file
       extractLibraryFile(nativeLibraryPath, nativeLibraryName, tempFolder)
@@ -373,7 +365,7 @@ object LArrayLoader {
 
 
     /**
-     * Get the snappy-java version by reading pom.properties embedded in jar.
+     * Get the LArray version by reading VERSION file embedded in jar.
      * This version data is used as a suffix of a dll file extracted from the
      * jar.
      * 
