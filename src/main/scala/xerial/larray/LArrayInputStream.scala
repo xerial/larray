b15dbb42328f0b578d8472f52d39e3a4c711cdaf
Taro L. Saito <leo@xerial.org>
2013-03-21 17:43:11 +0900

    Add LArrayInputStream #3

---
+++ src/main/scala/xerial/larray/LArrayInputStream.scala
@@ -0,0 +1,15 @@
+//--------------------------------------
+//
+// LArrayInputStream.scala
+// Since: 2013/03/21 5:28 PM
+// 
+//--------------------------------------
+
+package xerial.larray
+
+/**
+ * @author Taro L. Saito
+ */
+class LArrayInputStream {
+
+}
