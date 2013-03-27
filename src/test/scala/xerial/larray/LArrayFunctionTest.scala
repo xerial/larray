b63cd97968628e743331f855011e946101c5c05b
Taro L. Saito <leo@xerial.org>
2013-03-27 11:45:46 +0900

    Add LArray[Double] tests

--- src/test/scala/xerial/larray/LArrayFunctionTest.scala
+++ src/test/scala/xerial/larray/LArrayFunctionTest.scala
@@ -46,6 +46,154 @@ trait LIntArrayBehaviour { this: LArraySpec =>
 
   import LArrayFunctionTest._
 
+  def validDoubleArray(arr:Seq[Double]) {
+    val l: LArray[Double] = arr.toLArray
+
+    When(s"input is (${arr.mkString(", ")})")
+
+    "have iterator" in {
+      l.iterator === arr
+    }
+
+    "check status" in {
+      l.isEmpty should be (arr.isEmpty)
+      l.size shouldBe arr.size
+    }
+
+    "map elements" in {
+      l.map(_ * 2) === arr.map(_ * 2)
+      l.map(_.toInt) === arr.map(_.toInt)
+    }
+
+    "flatMap nested elements" in {
+      l.flatMap(x => (0 Until x.toInt).map(x => x)) === arr.flatMap(x => (0 until x.toInt).map(x => x))
+    }
+
+    "filter elements" in {
+      l.filter(_ % 2 == 1) === arr.filter(_ % 2 == 1)
+      l.filterNot(_ % 2 == 1) === arr.filterNot(_ % 2 == 1)
+    }
+
+    "reverse elements" in {
+      l.reverse === arr.reverse
+      l.reverseIterator.toLArray === arr.reverse
+    }
+
+    "find an element" taggedAs("fel") in {
+      l.find(_ == 4d) shouldBe arr.find(_ == 4d)
+      l.find(_ == 10d) shouldBe arr.find(_ == 10d)
+      l.contains(3d) should be(arr.contains(3d))
+      l.exists(_ == 1d) should be(arr.exists(_ == 1d))
+    }
+
+    "be used in for-comprehension" in {
+      l.map(x => x) === (for (e <- arr) yield e)
+      for (e <- l) yield e === (for (e <- arr) yield e)
+
+      for (e <- l if e > 3) yield e === (for (e <- arr if e > 3) yield e)
+    }
+
+    "slice elements" in {
+      l.slice(1, 3) === arr.slice(1, 3)
+      l.slice(2) === arr.slice(2, arr.length)
+    }
+
+    "drop elements" taggedAs ("drop") in {
+      val da = arr.drop(4)
+      debug(s"drop(4): ${da.mkString(", ")}")
+      l.drop(4) === arr.drop(4)
+    }
+
+    "report head/tail" in {
+      if(!l.isEmpty) {
+        l.head shouldBe arr.head
+        l.tail === arr.tail
+      }
+    }
+
+    "retrieve elements" in {
+      if(!l.isEmpty) {
+        l.init === arr.init
+      }
+      l.take(4) === arr.take(4)
+      l.takeRight(3) === arr.takeRight(3)
+      l.takeWhile(_ < 3) === arr.takeWhile(_ < 3)
+      l.dropWhile(_ < 4) === arr.dropWhile(_ < 4)
+    }
+
+    "split elements" in {
+      l.splitAt(2) === arr.splitAt(2)
+      l.splitAt(4) === arr.splitAt(4)
+      def f(x: Double) = x <= 2.0
+      l.span(f) === arr.span(f)
+      l.partition(_ % 3 == 0) === arr.partition(_ % 3 == 0)
+    }
+
+    "fold elements" in {
+      l.foldLeft(0d)(_ + _) shouldBe arr.foldLeft(0d)(_ + _)
+      (0d /: l)(_ + _) shouldBe ((0d /: arr)(_ + _))
+      l.foldRight(0d)(_ + _) shouldBe arr.foldRight(0d)(_ + _)
+      (l :\ 0d)(_ + _) shouldBe (arr :\ 0d)(_ + _)
+    }
+
+    "reduce elements" in {
+      def sum(a: Double, b: Double): Double = a + b
+      if(!l.isEmpty) {
+        l.reduce(sum) shouldBe arr.reduce(sum)
+        l.aggregate(100d)(sum, sum) shouldBe arr.aggregate(100d)(sum, sum)
+      }
+    }
+
+    "scan elements" in {
+      l.scanLeft(100d)(_ * _) === arr.scanLeft(100d)(_ * _)
+    }
+
+    "concatenate elements" in {
+      l ++ l === arr ++ arr
+      l.concat(l) === arr ++ arr
+      l :+ 10d === arr :+ 10d
+      7d +: l === 7d +: arr
+    }
+
+    "collect elements" in {
+      def f: PartialFunction[Double, Double] = {
+        case i: Double if i % 2 == 0 => i
+      }
+      l.collect(f) === arr.collect(f)
+      l.collectFirst(f) shouldBe arr.collectFirst(f)
+    }
+
+    "transform to array" in {
+      l.toArray shouldBe arr.toArray
+    }
+
+    "copy to array" in {
+      val b = new Array[Double](l.length.toInt)
+      l.copyToArray(b, 0, b.length)
+      b shouldBe arr.toArray
+    }
+
+    "zip with elements" in {
+      l.zipWithIndex === arr.zipWithIndex
+      l.zipAll(l.drop(3), -1, 255) === arr.zipAll(arr.drop(3), -1, 255)
+      l.zip(l.takeRight(4)) === arr.zip(arr.takeRight(4))
+    }
+
+    "make strings" in {
+      l.mkString("--") shouldBe arr.mkString("--")
+      l.mkString("(", ",", ")") shouldBe arr.mkString("(", ",", ")")
+    }
+
+    "read from/write to file" in {
+      val file = l.saveTo(File.createTempFile("sample", ".larray", new File("target")))
+      file.deleteOnExit()
+      LArray.loadFrom[Double](file) === arr
+    }
+
+  }
+
+
+
   def validFloatArray(arr:Seq[Float]) {
     val l: LArray[Float] = arr.toLArray
 
@@ -365,6 +513,10 @@ class LArrayFunctionTest extends LArraySpec with LIntArrayBehaviour {
 
   "float test2" should {
     behave like validFloatArray(Seq(3.5f, 1.2f, 2.9f, 0.3f, 3.8f, 7f))
+  }
+
+  "double test2" should {
+    behave like validDoubleArray(Seq(3.5d, 1.2d, 2.0d, 0.3d, 3.8d, 4d))
   }
 
 }
