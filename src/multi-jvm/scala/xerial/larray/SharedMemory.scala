9cf05ae88f07b3d76f209f4d925909d2e7386c20
Taro L. Saito <leo@xerial.org>
2013-04-10 00:29:29 +0900

    Add barrier implementation for multi-jvm testing

--- src/multi-jvm/scala/xerial/larray/SharedMemory.scala
+++ src/multi-jvm/scala/xerial/larray/SharedMemory.scala
@@ -1,53 +1,80 @@
 package xerial.larray
 
-import java.io.File
-import akka.remote.testkit.{MultiNodeSpec, MultiNodeSpecCallbacks, MultiNodeConfig}
-import org.scalatest.{BeforeAndAfterAll, WordSpec}
-import org.scalatest.matchers.MustMatchers
-import akka.testkit.ImplicitSender
+import java.io.{FileFilter, File}
+import xerial.core.log.Logger
+
 
 object SharedMemoryTest {
 
+
+
 }
 
-trait STMultiNodeSpec extends MultiNodeSpecCallbacks with LArraySpec {
+trait Barrier extends Logger {
 
-  override def beforeAll() = multiNodeSpecBeforeAll()
+  val numJVMs : Int
 
-  override def afterAll() = multiNodeSpecAfterAll()
-}
-
-object SharedMemorySpecConfig extends MultiNodeConfig {
-  val node1 = role("node1")
-  val node2 = role("node2")
-}
-
-class SharedMemorySpecMultiJvmNode1 extends SharedMemorySpec
-class SharedMemorySpecMultiJvmNode2 extends SharedMemorySpec
-
-class SharedMemorySpec extends MultiNodeSpec(SharedMemorySpecConfig)
- with STMultiNodeSpec with ImplicitSender {
-
-  import SharedMemorySpecConfig._
-  def initialParticipants = roles.size
-
-  "wait for all nodes to enter a barrier" in {
-    enterBarrier("startup")
+  val jvmID = {
+    val n = getClass.getSimpleName
+    val p = "[0-9]+".r
+    val id = p.findAllIn(n).toSeq.last.toInt
+    info(s"jvm ID:$id")
+    id
   }
 
-  "share memories between processes" in {
-    runOn(node1) {
+  def enterBarrier(name:String) {
+    debug(s"entering barrier:$name")
+    val f = new File(s"target/${name}.barrier")
+    f.deleteOnExit()
+    val m = LArray.mmap(f, 0, numJVMs, MMapMode.READ_WRITE)
+    m(jvmID -1) = 1.toByte
+    m.flush
+
+    def reached() : Boolean = {
+      m.forall(_ == 1.toByte)
+    }
+
+    while(!reached()) {
+      Thread.sleep(10)
+    }
+    debug(s"exit barrier: $name")
+  }
+
+}
+
+trait SharedMemorySpec extends LArraySpec with Barrier {
+  val numJVMs = 2
+
+  "cleanup barriers" in {
+    val barrierFiles = new File("target").listFiles(new FileFilter {
+      def accept(pathname: File) = {
+        pathname.getName.endsWith(".barrier")
+      }
+    })
+    for(f <- barrierFiles)
+      f.delete()
+  }
+
+}
+
+
+class SharedMemoryMultiJvm1 extends SharedMemorySpec {
+
+  "mmap" should {
+
+    "be able to shared between processes" in {
+
       info("preparing mmap")
 
       //val d = new File("/dev/shm")
       val f = new File(new File("target"), "sharedmemory.mmap")
-      f.getParentFile.mkdirs()
       //f.deleteOnExit
       val m = LArray.mmap(f, 0, 100, MMapMode.READ_WRITE)
       for(i <- 0 Until m.size)
         m(i) = i.toByte
 
       m.flush
+      debug(m.mkString(","))
 
       for(i <- 0 Until m.size) {
         if((i % 64) == 0)
@@ -56,14 +83,22 @@ class SharedMemorySpec extends MultiNodeSpec(SharedMemorySpecConfig)
           m(i) = 0.toByte
       }
 
-      enterBarrier("ready")
+      enterBarrier("prepare")
+
+      //m.close
+      info("done.")
     }
+  }
+}
 
-    runOn(node2) {
 
-      enterBarrier("ready")
+class SharedMemoryMultiJvm2 extends SharedMemorySpec {
+  "mmap" should {
+    "be able to shared between processes" in {
+
+      enterBarrier("prepare")
+
       val f = new File(new File("target"), "sharedmemory.mmap")
-      f.getParentFile.mkdirs()
       val m = LArray.mmap(f, 0, 100, MMapMode.READ_WRITE)
 
       debug(m.mkString(","))
@@ -71,9 +106,7 @@ class SharedMemorySpec extends MultiNodeSpec(SharedMemorySpecConfig)
       m(0) shouldBe (1.toByte)
       m(1) shouldBe (0.toByte)
       m(64) shouldBe (1.toByte)
+      //m.close
     }
-
-    enterBarrier("finished")
   }
 }
-
