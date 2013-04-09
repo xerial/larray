5fbe2c9282ed4b81c9a6db2c49d8226baceee418
Taro L. Saito <leo@xerial.org>
2013-04-09 13:15:32 +0900

    Fix file folder

--- src/multi-jvm/scala/xerial/larray/SharedMemory.scala
+++ src/multi-jvm/scala/xerial/larray/SharedMemory.scala
@@ -14,16 +14,16 @@ class SharedMemoryMultiJvm1 extends LArraySpec {
 
       info("preparing mmap")
 
-      val d = new File("/dev/shm")
-      val f = new File(if(d.exists) d else new File("target"), "sharedmemory.mmap")
+      //val d = new File("/dev/shm")
+      val f = new File(new File("target"), "sharedmemory.mmap")
       //f.deleteOnExit
-      val m = LArray.mmap(f, 0, 1024 * 1024, MMapMode.READ_WRITE)
+      val m = LArray.mmap(f, 0, 100, MMapMode.READ_WRITE)
       for(i <- 0 Until m.size)
         m(i) = i.toByte
 
       m.flush
 
-      //debug(m.mkString(","))
+      debug(m.mkString(","))
 
       for(i <- 0 Until m.size) {
         if((i % 64) == 0)
@@ -48,9 +48,9 @@ class SharedMemoryMultiJvm2 extends LArraySpec {
       Thread.sleep(1000)
 
       val f = new File(new File("target"), "sharedmemory.mmap")
-      val m = LArray.mmap(f, 0, 1024 * 1024, MMapMode.READ_WRITE)
+      val m = LArray.mmap(f, 0, 100, MMapMode.READ_WRITE)
 
-      //debug(m.mkString(","))
+      debug(m.mkString(","))
 
       m(0) shouldBe (1.toByte)
       m(1) shouldBe (0.toByte)
