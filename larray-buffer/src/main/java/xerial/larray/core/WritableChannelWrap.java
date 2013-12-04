8657a781260c5ac0c9cb9931cf79f4ca72a4a15f
Taro L. Saito <leo@xerial.org>
2013-12-04 11:47:32 +0900

    Remove DirectBuffer class cast.

--- larray-buffer/src/main/java/xerial/larray/core/WritableChannelWrap.java
+++ larray-buffer/src/main/java/xerial/larray/core/WritableChannelWrap.java
@@ -1,7 +1,5 @@
 package xerial.larray.core;
 
-import sun.nio.ch.DirectBuffer;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
@@ -24,9 +22,8 @@ class WritableChannelWrap implements WritableByteChannel {
     public int write(ByteBuffer src) throws IOException {
         int len = (int) Math.max(src.limit() - src.position(), 0);
         int writeLen = 0;
-        if(src.getClass().isAssignableFrom(DirectBuffer.class)) {
-            DirectBuffer d = (DirectBuffer) src;
-            unsafe.copyMemory(d.address() + src.position(), cursor, len);
+        if(src.isDirect()) {
+            src.put(b.toDirectByteBuffer(cursor, len));
             writeLen = len;
         }
         else if(src.hasArray()) {
